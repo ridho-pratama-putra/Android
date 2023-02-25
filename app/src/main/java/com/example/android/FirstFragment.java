0634411fa0a1a79321dd3f8f313f5484567d98b7
ridho <ridhopratamavd@gmail.com>
2023-02-25 12:39:29 +0700

    notification received but no audio/vibration

--- app/src/main/java/com/example/android/FirstFragment.java
+++ app/src/main/java/com/example/android/FirstFragment.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
